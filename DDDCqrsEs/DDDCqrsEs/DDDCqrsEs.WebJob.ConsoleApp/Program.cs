e1811299f888862bc3b125d31e9e420109f8ff7d
Adelina Ionescu <19734500+adelinaoana@users.noreply.github.com>
2023-06-13 14:47:31 +0300

    Added new WebJob Trigger

---
+++ DDDCqrsEs/DDDCqrsEs/DDDCqrsEs.WebJob.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using DDDCqrsEs.Application.EventHandlers;
+using DDDCqrsEs.Domain.Events;
+using DDDCqrsEs.Persistance;
+using DDDCqrsEs.Persistance.Bootstrap;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DDDCqrsEs.WebJob.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddServiceBus();
+            });
+            builder.ConfigureServices(services =>
+            {
+                services.AddLogging();
+                services.AddMediatR(typeof(StockEventHandler).Assembly, typeof(BaseEvent).Assembly);
+                services.AddDbContext<ToDoDbContext>(options => options.UseSqlServer(configurationBuilder.GetConnectionString("DbString")));
+                services.RegisterRepositories();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
---
+++ DDDCqrsEs/DDDCqrsEs/DDDCqrsEs.WebJob.ConsoleApp/WebJobFunction.cs
@@ -0,0 +1,37 @@
+﻿using DDDCqrsEs.Domain.Events;
+using MediatR;
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+
+namespace DDDCqrsEs.WebJob.ConsoleApp
+{
+    public class WebJobFunction
+    {
+        private readonly IMediator _mediator;
+
+        public WebJobFunction(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Run([ServiceBusTrigger("stockevents",
+                Connection = "ConnectionStrings:ServiceBusString")]
+            string myQueueItem)
+        {
+            var _event = JsonConvert.DeserializeObject<BaseEvent>(myQueueItem);
+
+            var assembly = typeof(BaseEvent).Assembly;
+            var _namespace = typeof(BaseEvent).Namespace;
+            var fullClassName = _namespace + "." + _event.EventType;
+            var eventType = assembly.GetType(fullClassName);
+            dynamic typedEvent = Activator.CreateInstance(eventType);
+
+            typedEvent.Stock = _event.Stock;
+            typedEvent.EventType = _event.EventType;
+            typedEvent.AggregateId = _event.AggregateId;
+            typedEvent.Version = _event.Version;
+
+            await _mediator.Send(typedEvent);
+        }
+    }
+}
